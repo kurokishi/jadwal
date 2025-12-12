34a2f0e278c8bfdd3aae75aded3ff41d45dc0f40
kurokishi <kuroreidho@gmail.com>
2025-12-12 13:57:15 +0700

    Update __init__.py

--- app/ui/__init__.py
+++ app/ui/__init__.py
@@ -1,12 +1,17 @@
-"""
-UI components package.
+# app/ui/__init__.py
 
-Contains Streamlit UI tabs and controls:
-- tab_upload
-- tab_analyzer
-- tab_kanban_drag
-- tab_settings
-- tab_visualization
-- sidebar
-"""
+from .tab_upload import render_upload_tab
+from .tab_analyzer import render_analyzer_tab
+from .tab_visualization import render_visualization_tab
+from .tab_settings import render_settings_tab
+from .tab_kanban_drag import render_drag_kanban
+from .sidebar import render_sidebar
 
+__all__ = [
+    "render_upload_tab",
+    "render_analyzer_tab",
+    "render_visualization_tab",
+    "render_settings_tab",
+    "render_drag_kanban",
+    "render_sidebar"
+]
