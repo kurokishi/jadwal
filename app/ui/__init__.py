d0676027002b4a3b4b830c0d196d94e798c94db3
kurokishi <kuroreidho@gmail.com>
2025-12-12 15:20:34 +0700

    Update __init__.py

--- app/ui/__init__.py
+++ app/ui/__init__.py
@@ -1,17 +1,17 @@
 # app/ui/__init__.py
 
+from .sidebar import render_sidebar
 from .tab_upload import render_upload_tab
 from .tab_analyzer import render_analyzer_tab
 from .tab_visualization import render_visualization_tab
 from .tab_settings import render_settings_tab
 from .tab_kanban_drag import render_drag_kanban
-from .sidebar import render_sidebar
 
 __all__ = [
+    "render_sidebar",
     "render_upload_tab",
     "render_analyzer_tab",
     "render_visualization_tab",
     "render_settings_tab",
-    "render_drag_kanban",
-    "render_sidebar"
+    "render_drag_kanban"
 ]
